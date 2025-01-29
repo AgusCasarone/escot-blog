d3e2e0ff167894753139b971887a982789f9f5e8
Agustina Casarone <agus.casarone@gmail.com>
2025-01-29 00:39:50 -0300

    impl post and find basics

---
+++ src/articles/dto/create-article.dto.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+
+export class CreateArticleDto {
+    readonly title: string;
+    readonly content: string;
+    readonly author: string;
+    readonly image: string;
+}
