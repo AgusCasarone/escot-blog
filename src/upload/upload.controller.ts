21e277a78e01453a26345769f90c17c0899dc0f2
Agustina Casarone <agus.casarone@gmail.com>
2025-01-29 21:33:57 -0300

    impl image upload

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,13 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable prettier/prettier */
 import { Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadService } from './upload.service';
 
 @Controller('upload')
 export class UploadController {
 
+    constructor(
+        private readonly uploadService: UploadService
+    ){}
+
     @Post()
     @UseInterceptors(FileInterceptor('file'))
-    uploadFile(@UploadedFile(
+    async uploadFile(@UploadedFile(
         new ParseFilePipe({
             validators: [
                 new MaxFileSizeValidator({ maxSize: 1000000 }),
@@ -15,7 +22,7 @@ export class UploadController {
             ]
         })
     ) file: Express.Multer.File) {
-        
+        return await this.uploadService.upload(file.originalname, file.buffer);
     }
 
 }
